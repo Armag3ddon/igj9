42a2ae01809baa7cc41059c03da64549d5cabe2b
Percepter <the_percepter@web.de>
2015-11-07 10:43:24 +0100

    -

--- js/scenes/MapScene.js
+++ js/scenes/MapScene.js
@@ -4,8 +4,6 @@ function MapScene( definition ) {
 	this.definition = definition;
 };
 
-var mapBuffer = false;
-
 MapScene.prototype = new Scene;
 
 MapScene.prototype.drawBase = MapScene.prototype.draw;
@@ -14,39 +12,57 @@ MapScene.prototype.draw = function ( ctx ) {
 	var wdt = this.definition.tilewidth;
 	var hgt = this.definition.tileheight;
 
-	if(mapBuffer == false) {
+	for (var i = 0; i < this.definition.layers.length; i++) {
 
-		for (var i = 0; i < this.definition.layers.length; i++) {
+		if (!this.definition.layers[i].visible) continue;
 
-			if (!this.definition.layers[i].visible) continue;
+		for (var j = 0; j < this.definition.layers[i].data.length; j++) {
+			var tile = this.definition.layers[i].data[j];
 
-			for (var j = 0; j < this.definition.layers[i].data.length; j++) {
-				var tile = this.definition.layers[i].data[j];
+			if (tile == 0) continue;
 
-				if (tile == 0) continue;
+			var gWdt = this.definition.tilesets[0].tilewidth;
+			var gHgt = this.definition.tilesets[0].tileheight;
 
-				var gWdt = this.definition.tilesets[0].tilewidth;
-				var gHgt = this.definition.tilesets[0].tileheight;
+			var columns = this.definition.tilesets[0].imagewidth / gWdt;
 
-				var columns = this.definition.tilesets[0].imagewidth / gWdt;
+			var gX = gWdt * (tile % columns) - gWdt;
+			var gY = gHgt * Math.floor(tile / columns);
 
-				var gX = gWdt * (tile % columns) - gWdt;
-				var gY = gHgt * Math.floor(tile / columns);
+			var tX = gWdt * (j % this.definition.layers[i].width);
+			var tY = gHgt * Math.floor(j / this.definition.layers[i].width);
 
-				var tX = gWdt * (j % this.definition.layers[i].width);
-				var tY = gHgt * Math.floor(j / this.definition.layers[i].width);
-
-				this.tileset.area(ctx, gX, gY, gWdt, gHgt, tX, tY);
-			}
+			this.tileset.area(ctx, gX, gY, gWdt, gHgt, tX, tY);
 		}
-		mapBuffer = ctx;
-
-		//console.log("draw everything");
-	} else {
-		ctx = mapBuffer;
-
-		//console.log("draw mapBuffer");
 	}
 
 	this.drawBase(ctx);
+
+	//experiment ->
+
+	/*var newTileset1 = new AnimationSprite('img/character_black_yellow_blue.png', 4);
+	var newTileset2 = new AnimationSprite('img/character_blonde_red_black.png', 4);
+	var newTileset3 = new AnimationSprite('img/character_hat_black_beige.png', 4);
+	var newTileset4 = new AnimationSprite('img/character_brown_blue_brown.png',4);
+
+	var cX = 100;
+	var cY = 100;
+
+	var cWdtF = 144;
+	var cHgtF = 288;
+
+	var cWdt = cWdtF / 4;	//36
+	var cHgt = cHgtF / 4;	//72
+
+	var cHeadHgt = 24;
+	var cChestHgt = 23;
+	var cPantHgt = 20;
+	var ctx2 = mapBuffer;
+
+	newTileset1.draw(ctx2, 0, 288 / 2, 1);
+	newTileset2.draw(ctx2, 0, 288 / 2 + cHeadHgt, 1);
+	newTileset3.draw(ctx2, 0, 288 / 2 + cHeadHgt + cChestHgt, 1);
+	//newTileset4.area(ctx2, 0, 288 / 2, cWdt, cHgt, 150, 100);
+
+	//experiment <-*/
 }
