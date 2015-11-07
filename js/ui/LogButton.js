7731a66911d40879b49f9f4d05c1d60dad136972
Percepter <the_percepter@web.de>
2015-11-08 00:30:10 +0100

    fail

--- js/ui/LogButton.js
+++ js/ui/LogButton.js
@@ -1,9 +1,10 @@
 function LogButton()
 {
-	this.img = new Sprite( 'img/LogButton.png' );
+	//this.img = new Sprite( 'img/LogButton.png' );
 	this.x = 980;
 	this.y = 0;
-	this.area = new Rect( new V2( this.x, this.y ), new V2( this.x+this.img.width, this.y+this.img.height ));
+	//this.area = new Rect( new V2( this.x, this.y ), new V2( this.x+this.img.width, this.y+this.img.height ));
+	this.area = new Rect( new V2( this.x, this.y ), new V2( this.x+300, this.y+300 ));
 
 	this.opened = false;
 
