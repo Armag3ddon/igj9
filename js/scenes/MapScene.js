5bfd9c3df62f4e8ae728194ce913d56dc846f9be
Percepter <the_percepter@web.de>
2015-11-07 12:04:20 +0100

    old

--- js/scenes/MapScene.js
+++ js/scenes/MapScene.js
@@ -38,31 +38,4 @@ MapScene.prototype.draw = function ( ctx ) {
 
 	this.drawBase(ctx);
 
-	//experiment ->
-
-	/*var newTileset1 = new AnimationSprite('img/character_black_yellow_blue.png', 4);
-	var newTileset2 = new AnimationSprite('img/character_blonde_red_black.png', 4);
-	var newTileset3 = new AnimationSprite('img/character_hat_black_beige.png', 4);
-	var newTileset4 = new AnimationSprite('img/character_brown_blue_brown.png',4);
-
-	var cX = 100;
-	var cY = 100;
-
-	var cWdtF = 144;
-	var cHgtF = 288;
-
-	var cWdt = cWdtF / 4;	//36
-	var cHgt = cHgtF / 4;	//72
-
-	var cHeadHgt = 24;
-	var cChestHgt = 23;
-	var cPantHgt = 20;
-	var ctx2 = mapBuffer;
-
-	newTileset1.draw(ctx2, 0, 288 / 2, 1);
-	newTileset2.draw(ctx2, 0, 288 / 2 + cHeadHgt, 1);
-	newTileset3.draw(ctx2, 0, 288 / 2 + cHeadHgt + cChestHgt, 1);
-	//newTileset4.area(ctx2, 0, 288 / 2, cWdt, cHgt, 150, 100);
-
-	//experiment <-*/
 }
