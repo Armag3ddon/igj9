208969cdf46d143552e9baa637719faa55eb61a6
leFroxx <runge.ph@gmail.com>
2015-11-08 12:26:44 +0100

    LogButton

--- js/ui/LogButton.js
+++ js/ui/LogButton.js
@@ -1,14 +1,24 @@
 function LogButton()
 {
-	//this.img = new Sprite( 'img/LogButton.png' );
 	this.x = 980;
 	this.y = 0;
-	//this.area = new Rect( new V2( this.x, this.y ), new V2( this.x+this.img.width, this.y+this.img.height ));
-	this.area = new Rect( new V2( this.x, this.y ), new V2( this.x+300, this.y+300 ));
+	var anchorStart = new V2( this.x, this.y );
+	var anchorEnd = new V2( this.x + 300, this.y + 300 );
+	this.area = new Rect( anchorStart, anchorEnd );
+	
+	// popup
+	var popupP1 = new V2(305, 50);
+	var popupP2 = new V2(popupP1.x + 670, popupP1.y + 620);
+	this.popup = new Rect(popupP1, popupP2);
+	console.log(this.popup);
 
 	this.opened = false;
+	
+	this.amountOfLines = 0;
 
-	this.entities = [];
+	this.entities = [ new LogEntry(this, "I'm a test"),
+					new LogEntry(this, "Me too!"),
+					new LogEntry(this, "And I'm a third one asdlkjfgasdljfgsaksjgfaskgfkasjdgfsakjgfs")];
 }
 
 LogButton.prototype.open = function() {
@@ -20,12 +30,12 @@ LogButton.prototype.close = function() {
 }
 
 LogButton.prototype.draw = function( ctx ) {
-	//this.img.draw(ctx, this.x, this.y);
-
 	if (this.opened)
 	{
 		ctx.fillStyle = '#ffffff';
-		ctx.fillRect(305,50, 670,620);
+		var width = this.popup.p2.x - this.popup.p1.x;
+		var height = this.popup.p2.y - this.popup.p1.y;
+		ctx.fillRect(this.popup.p1.x, this.popup.p1.y, width, height);
 		for( var i = 0; i < this.entities.length; i++ )
 			if( this.entities[i].draw )
 				this.entities[i].draw( ctx );
@@ -40,3 +50,44 @@ LogButton.prototype.click = function( pos ) {
 			this.open();
 	}
 };
+
+LogButton.prototype.addLog = function(log) {
+	if (typeof log == 'LogEntry') {
+		this.entities.push(log);
+	}
+}
+
+function LogEntry(logButton, text) {
+	this.logButton = logButton;
+	this.maxLength = 36;
+	this.margin = 10;
+	var lines = [];
+	var ind = 0;
+	for (var i = 0, charsLength = text.length; i < charsLength; i += this.maxLength) {
+		lines[ind] = {};
+		lines[ind].text = text.substring(i, i + this.maxLength);
+	
+		var positionX = this.logButton.popup.p1.x + this.margin;
+		var yAdd = this.logButton.amountOfLines*config.fontsize;
+		var positionY = this.logButton.popup.p1.y + yAdd;
+		var position = new V2(positionX, positionY);
+		lines[ind].position = position;
+		ind++;
+		this.logButton.amountOfLines++;
+	}
+	this.lines = lines;
+	if (this.lines.length) {
+		this.logButton.amountOfLines += this.lines.length;
+	}
+	
+	console.log(this);
+}
+
+LogEntry.prototype.draw = function( ctx ) {
+	ctx.fillStyle = '#ff0000';
+	ctx.font = '20px Verdana';
+	for (ind = 0; ind < this.lines.length; ind++) {
+		var line = this.lines[ind];
+		ctx.fillText( line.text, line.position.x, line.position.y + config.fontsize + this.margin);
+	}
+}
