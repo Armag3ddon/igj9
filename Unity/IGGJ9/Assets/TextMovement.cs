54d2a1371d568c36ec69a398811842639de8f1c1
Marcel Zons <marcel.zons@innogames.com>
2015-11-08 11:59:06 +0100

    Stuff

--- Unity/IGGJ9/Assets/TextMovement.cs
+++ Unity/IGGJ9/Assets/TextMovement.cs
@@ -3,16 +3,52 @@ using System.Collections;
 
 public class TextMovement : MonoBehaviour {
 
-    public float moveSpeed = 10f;
+    public float moveSpeed = 75f;
 
     private Vector2 input;
     private bool isMoving = false;
     private float factor;
+    private int lockX = 0;
+    private int lockY = 0;
+    private Animation animationCommand = null;
+    //CharacterController controller = null;
+
+    void Start()
+    {
+        animationCommand = gameObject.GetComponent<Animation>();
+        //controller = GetComponent<CharacterController>();
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject != null)
+        {
+            if (input.x > 0)
+            {
+                lockX = 1;
+            }
+            else if (input.x < 0)
+            {
+                lockX = -1;
+            }
+
+            if (input.y > 0)
+            {
+                lockY = 1;
+            }
+            else if (input.y < 0)
+            {
+                lockY = -1;
+            }
+        }
+    }
 
     public void FixedUpdate()
     {
+        
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-           
+
         if (input != Vector2.zero)
         {
             if (input.x != 0 && input.y != 0)
@@ -24,25 +60,37 @@ public class TextMovement : MonoBehaviour {
                 factor = 1f;
             }
 
-            if (input.x > 0)
+            if (input.x > 0 && lockX < 1)
             {
                 transform.position += transform.right * moveSpeed * Time.deltaTime * factor;
+                lockX = 0;
             }
-            else if (input.x < 0)
+            else if (input.x < 0 && lockX > -1)
             {
                 transform.position += -1f * transform.right * moveSpeed * Time.deltaTime * factor;
+                lockX = 0;
             }
 
-            if (input.y > 0)
+            if (input.y > 0 && lockY < 1)
             {
                 transform.position += transform.up * moveSpeed * Time.deltaTime * factor;
+                lockY = 0;
             }
-            else if (input.y < 0)
+            else if (input.y < 0 && lockY > -1)
             {
                 transform.position += -1f * transform.up * moveSpeed * Time.deltaTime * factor;
-            }
+                lockY = 0;
+                animationCommand.Play("main_character_walk_south");
+                Debug.Log("");
 
+            }
         }
+    }
+
+    void OnCollisionExit2D (Collision2D collision)
+    {
+        lockX = 0;
+        lockY = 0;
     }
         //endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
         //  startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
