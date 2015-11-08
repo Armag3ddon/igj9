dd1b4551e0aec39ee1577d1ccbe2b8b3e61ad61e
Percepter <the_percepter@web.de>
2015-11-08 14:41:52 +0100

    new maps

--- data/city.js
+++ data/city.js
@@ -1,8 +1,8 @@
 var city = {
-	districtCount: 5,
+	districtCount: 11,
 	district1: {
-		posX: 400,
-		posY: 68,
+		posX: 211,
+		posY: 3,
 		background: 'District1.png',
 		iconX: 0,
 		iconY: 235,
@@ -12,8 +12,8 @@ var city = {
 		disHgt: 229
 	},
 	district2: {
-		posX: 173,
-		posY: 88,
+		posX: 604,
+		posY: 140,
 		background: 'District2.png',
 		iconX: 55,
 		iconY: 235,
@@ -23,8 +23,8 @@ var city = {
 		disHgt: 241
 	},
 	district3: {
-		posX: 252,
-		posY: 238,
+		posX: 474,
+		posY: 115,
 		background: 'District3.png',
 		iconX: 110,
 		iconY: 235,
@@ -34,8 +34,8 @@ var city = {
 		disHgt: 242
 	},
 	district4: {
-		posX: 474,
-		posY: 140,
+		posX: 479,
+		posY: 81,
 		background: 'District4.png',
 		iconX: 165,
 		iconY: 235,
@@ -45,8 +45,8 @@ var city = {
 		disHgt: 407
 	},
 	district5: {
-		posX: 211,
-		posY: 0,
+		posX: 470,
+		posY: 81,
 		background: 'District5.png',
 		iconX: 165,
 		iconY: 235,
@@ -56,9 +56,9 @@ var city = {
 		disHgt: 175
 	},
 	district6: {
-		posX: 400,
-		posY: 68,
-		background: 'District1.png',
+		posX: 278,
+		posY: 237,
+		background: 'District6.png',
 		iconX: 0,
 		iconY: 235,
 		iconWidth: 55,
@@ -67,9 +67,9 @@ var city = {
 		disHgt: 229
 	},	
 	district7: {
-		posX: 400,
-		posY: 68,
-		background: 'District1.png',
+		posX: 252,
+		posY: 251,
+		background: 'District7.png',
 		iconX: 0,
 		iconY: 235,
 		iconWidth: 55,
@@ -78,9 +78,9 @@ var city = {
 		disHgt: 229
 	},
 	district8: {
-		posX: 173,
+		posX: 280,
 		posY: 88,
-		background: 'District2.png',
+		background: 'District8.png',
 		iconX: 55,
 		iconY: 235,
 		iconWidth: 55,
@@ -89,9 +89,9 @@ var city = {
 		disHgt: 241
 	},
 	district9: {
-		posX: 252,
-		posY: 238,
-		background: 'District3.png',
+		posX: 173,
+		posY: 144,
+		background: 'District9.png',
 		iconX: 110,
 		iconY: 235,
 		iconWidth: 55,
@@ -100,9 +100,9 @@ var city = {
 		disHgt: 242
 	},
 	district10: {
-		posX: 474,
-		posY: 140,
-		background: 'District4.png',
+		posX: 396,
+		posY: 112,
+		background: 'District10.png',
 		iconX: 165,
 		iconY: 235,
 		iconWidth: 55,
@@ -111,9 +111,9 @@ var city = {
 		disHgt: 407
 	},
 	district11: {
-		posX: 211,
-		posY: 0,
-		background: 'District5.png',
+		posX: 503,
+		posY: 156,
+		background: 'District11.png',
 		iconX: 165,
 		iconY: 235,
 		iconWidth: 55,
